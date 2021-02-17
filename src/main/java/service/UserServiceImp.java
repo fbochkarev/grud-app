addb369b8f896f73fb41e8d18914944eb7d9e50a
fz.bochkarev <H_y678ik>
2021-02-17 09:32:52 +0200

    CRUD-приложение на Spring Java pre-project. Практическая задача 2.3.1 v2 transferring transactions to the service

--- src/main/java/service/UserServiceImp.java
+++ src/main/java/service/UserServiceImp.java
@@ -1,38 +1,57 @@
 package service;
 
-import dao.UserDao;
-import dao.UserDaoImp;
 import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.List;
 
 @Service
 public class UserServiceImp implements UserService {
-    UserDao userDao = new UserDaoImp();
+    @Autowired
+    private EntityManagerFactory emf;
+
+    public EntityManager getEntityManager() {
+        emf = Persistence.createEntityManagerFactory("grud-app");
+        return emf.createEntityManager();
+    }
 
     @Override
     public void add(User user) {
-        userDao.add(user);
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        em.merge(user);
+        em.getTransaction().commit();
     }
 
     @Override
     public void removeUser(long id) {
-        userDao.removeUser(id);
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        User user = em.find(User.class, id);
+        em.remove(user);
+        em.getTransaction().commit();
     }
 
     @Override
     public void updateUser(User user) {
-        userDao.updateUser(user);
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        em.merge(user);
+        em.getTransaction().commit();
     }
 
     @Override
     public User getUserById(long id) {
-        return userDao.getUserById(id);
+        EntityManager em = getEntityManager();
+        return em.getReference(User.class, id);
     }
 
     @Override
     public List<User> listUsers() {
-        return userDao.listUsers();
+        return getEntityManager().createQuery("from User").getResultList();
     }
 }
